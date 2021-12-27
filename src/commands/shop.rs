0fa2778cca51ea34900b28972cdc689c2931ad62
grostaco <grostaco@gmail.com>
2021-12-27 23:06:12 +0700

    feat: add from_file

--- src/commands/shop.rs
+++ src/commands/shop.rs
@@ -1,10 +1,27 @@
-pub struct Shop {
-    items: Vec<Item>,
-}
+use csv::Reader;
+use serde::{Deserialize, Serialize};
 
+pub struct Shop(Vec<Item>);
+
+#[derive(Serialize, Deserialize)]
 struct Item {
     name: String,
-    cost: u8,
+    cost: u64,
 }
 
-impl Shop {}
+impl Shop {
+    pub fn new() -> Self {
+        Self(Vec::new())
+    }
+
+    pub fn from_file(shop_file: &str) -> Result<(), csv::Error> {
+        let mut shop = Shop::new();
+        let mut rdr = Reader::from_path(shop_file)?;
+
+        for record in rdr.deserialize() {
+            shop.0.push(record?);
+        }
+
+        Ok(())
+    }
+}
